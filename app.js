78c25eed61edba089a61a925a99af6bd99d51e2a
dodoschnau <huangmeisin@gmail.com>
2024-04-23 12:20:22 +0800

    buid: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('listing restaurants')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
 })
 
 app.listen(port, () => {
