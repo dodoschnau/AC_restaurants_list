3956ecfc01b5e5304912ab6dafe8ccc37ee8ac36
dodoschnau <huangmeisin@gmail.com>
2024-06-05 19:16:59 +0800

    feat: add error handling

--- app.js
+++ app.js
@@ -37,121 +37,188 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  const keyword = req.query.keyword?.trim()
-  const sort = req.query.sort
+  try {
+    const keyword = req.query.keyword?.trim()
+    const sort = req.query.sort
 
-  let order = []
-  switch (sort) {
-    case 'nameAsc':
-      order.push(['name', 'ASC']);
-      break;
-    case 'nameDesc':
-      order.push(['name', 'DESC']);
-      break;
-    case 'category':
-      order.push(['category', 'ASC']);
-      break;
-    case 'location':
-      order.push(['location', 'ASC']);
-      break;
-    default:
-      order.push(['id', 'ASC']);
-      break;
-  }
+    let order = []
+    switch (sort) {
+      case 'nameAsc':
+        order.push(['name', 'ASC']);
+        break;
+      case 'nameDesc':
+        order.push(['name', 'DESC']);
+        break;
+      case 'category':
+        order.push(['category', 'ASC']);
+        break;
+      case 'location':
+        order.push(['location', 'ASC']);
+        break;
+      default:
+        order.push(['id', 'ASC']);
+        break;
+    }
 
-
-  return Restaurant.findAll({
-    attributes: ['id', 'image', 'name', 'category', 'rating', 'location'],
-    order,
-    raw: true
-  })
-
-    .then((restaurants) => {
-      const matchedRestaurants = keyword ? restaurants.filter((store) =>
-        Object.values(store).some((content) => {
-          if (typeof content === 'string') {
-            return content.toLowerCase().includes(keyword.toLowerCase())
-          }
-          return false
-        })
-      ) : restaurants
-      res.render('index', { restaurants: matchedRestaurants, keyword, sort, message: req.flash('success') })
+    return Restaurant.findAll({
+      attributes: ['id', 'image', 'name', 'category', 'rating', 'location'],
+      order,
+      raw: true
     })
 
-    .catch((err) => console.log(err))
+      .then((restaurants) => {
+        const matchedRestaurants = keyword ? restaurants.filter((store) =>
+          Object.values(store).some((content) => {
+            if (typeof content === 'string') {
+              return content.toLowerCase().includes(keyword.toLowerCase())
+            }
+            return false
+          })
+        ) : restaurants
+        res.render('index', { restaurants: matchedRestaurants, keyword, sort, message: req.flash('success'), error: req.flash('error') })
+      })
+
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '資料取得失敗')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 app.get('/restaurants/new', (req, res) => {
-  res.render('new')
+  try {
+    res.render('new', { error: req.flash('error') })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 
 app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'name_en', 'category', 'rating', 'location', 'googlemap', 'phone', 'description', 'image'],
-    raw: true
-  })
-    .then((restaurant) => res.render('detail', { restaurant, message: req.flash('success') }))
-    .catch((err) => console.log(err))
+  try {
+    const id = req.params.id
+    return Restaurant.findByPk(id, {
+      attributes: ['id', 'name', 'name_en', 'category', 'rating', 'location', 'googlemap', 'phone', 'description', 'image'],
+      raw: true
+    })
+      .then((restaurant) => res.render('detail', { restaurant, message: req.flash('success'), error: req.flash('error') }))
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '資料取得失敗')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'name_en', 'category', 'rating', 'location', 'googlemap', 'phone', 'description', 'image'],
-    raw: true
-  })
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch((err) => console.log(err))
+  try {
+    const id = req.params.id
+    return Restaurant.findByPk(id, {
+      attributes: ['id', 'name', 'name_en', 'category', 'rating', 'location', 'googlemap', 'phone', 'description', 'image'],
+      raw: true
+    })
+      .then((restaurant) => res.render('edit', { restaurant, error: req.flash('error') }))
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '讀取網頁失敗')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const googlemap = req.body.googlemap
-  const rating = req.body.rating
-  const description = req.body.description
+  try {
+    const name = req.body.name
+    const name_en = req.body.name_en
+    const category = req.body.category
+    const image = req.body.image
+    const location = req.body.location
+    const phone = req.body.phone
+    const googlemap = req.body.googlemap
+    const rating = req.body.rating
+    const description = req.body.description
 
-  return Restaurant.create({ name, name_en, category, image, location, phone, googlemap, rating, description })
-    .then(() => {
-      req.flash('success', '新增成功！')
-      return res.redirect('/restaurants')
-    })
-    .catch((err) => { console.log(err) })
+    return Restaurant.create({ name, name_en, category, image, location, phone, googlemap, rating, description })
+      .then(() => {
+        req.flash('success', '新增成功！')
+        return res.redirect('/restaurants')
+      })
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '新增失敗:(')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const googlemap = req.body.googlemap
-  const rating = req.body.rating
-  const description = req.body.description
+  try {
+    const id = req.params.id
+    const name = req.body.name
+    const name_en = req.body.name_en
+    const category = req.body.category
+    const image = req.body.image
+    const location = req.body.location
+    const phone = req.body.phone
+    const googlemap = req.body.googlemap
+    const rating = req.body.rating
+    const description = req.body.description
 
-  return Restaurant.update({ name, name_en, category, image, location, phone, googlemap, rating, description }, { where: { id } })
-    .then(() => {
-      req.flash('success', '編輯成功！')
-      return res.redirect(`/restaurants/${id}`)
-    })
-    .catch((err) => { console.log(err) })
+    return Restaurant.update({ name, name_en, category, image, location, phone, googlemap, rating, description }, { where: { id } })
+      .then(() => {
+        req.flash('success', '編輯成功！')
+        return res.redirect(`/restaurants/${id}`)
+      })
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '編輯失敗:(')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
 })
 
 app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.destroy({ where: { id } })
-    .then(() => {
-      req.flash('success', '刪除成功！')
-      return res.redirect('/restaurants')
-    })
+  try {
+    const id = req.params.id
+    return Restaurant.destroy({ where: { id } })
+      .then(() => {
+        req.flash('success', '刪除成功！')
+        return res.redirect('/restaurants')
+      })
+      .catch((error) => {
+        console.error(error)
+        req.flash('error', '刪除失敗:(')
+        return res.redirect('back')
+      })
+  } catch (error) {
+    console.error(error)
+    req.flash('error', '伺服器錯誤')
+    return res.redirect('back')
+  }
+
 })
 
 app.listen(port, () => {
