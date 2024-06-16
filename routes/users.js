d21c2a35e192e78a63742feee6aae7715a9d5edb
dodoschnau <huangmeisin@gmail.com>
2024-06-16 17:57:04 +0800

    feat: implement register

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,41 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../models')
+const User = db.User
 
 // Register
-router.post('/', (req, res) => {
-  res.send('POST Register')
+router.post('/', (req, res, next) => {
+  const { userName, email, password, confirmPassword } = req.body
+
+  if (!email || !password) {
+    req.flash('error', 'email及password為必填！')
+    return res.redirect('back')
+  }
+  if (password !== confirmPassword) {
+    req.flash('error', '密碼與驗證密碼不相符')
+    return res.redirect('back')
+  }
+
+  return User.count({ where: { email } })
+    .then((rowCount) => {
+      if (rowCount > 0) {
+        req.flash('error', '該email已被註冊！')
+        return
+      }
+      return User.create({ userName, email, password })
+    })
+    .then((user) => {
+      if (!user) {
+        return res.redirect('back')
+      }
+      req.flash('success', '註冊成功！')
+      return res.redirect('/login')
+    })
+    .catch((error) => {
+      error.errorMassage = '註冊失敗:('
+      next(error)
+    })
 })
 
 module.exports = router
